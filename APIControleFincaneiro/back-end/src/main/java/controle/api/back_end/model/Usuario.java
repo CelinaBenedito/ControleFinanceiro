ab45256ff9e269d8d9b8be55443000e5b1032441
Celina dos Santos Benedito <celina.benedito@avanade.com>
2026-03-24 15:15:25 -0300

    FEAT: separando o properties e adicionado regra de negócio do usuario

--- APIControleFincaneiro/back-end/src/main/java/controle/api/back_end/model/Usuario.java
+++ APIControleFincaneiro/back-end/src/main/java/controle/api/back_end/model/Usuario.java
@@ -1,7 +1,12 @@
 package controle.api.back_end.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -9,6 +14,19 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Size(max = 50)
+    @NotBlank
     private String nome;
+    @Size(max = 100)
+    @NotBlank
+    @Column(nullable = false)
+    private String sobrenome;
+    @Past
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UsuarioSexo sexo;
+
 
 }
---
+++ APIControleFincaneiro/back-end/src/main/java/controle/api/back_end/model/UsuarioSexo.java
@@ -0,0 +1,7 @@
+package controle.api.back_end.model;
+
+public enum UsuarioSexo {
+    Feminino,
+    Masculino,
+    PrefiroNãoIdentificar
+}
