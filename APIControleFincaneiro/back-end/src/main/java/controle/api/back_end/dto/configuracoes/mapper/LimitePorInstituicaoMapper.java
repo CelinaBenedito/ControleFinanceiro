f77e0513c284cac645eea26d62f9116134b6e741
Celina dos Santos Benedito <celina.benedito@avanade.com>
2026-04-14 11:05:48 -0300

    Feat: refatorando

--- APIControleFincaneiro/back-end/src/main/java/controle/api/back_end/dto/configuracoes/mapper/LimitePorInstituicaoMapper.java
+++ APIControleFincaneiro/back-end/src/main/java/controle/api/back_end/dto/configuracoes/mapper/LimitePorInstituicaoMapper.java
@@ -1,10 +1,12 @@
 package controle.api.back_end.dto.configuracoes.mapper;
 
+import controle.api.back_end.dto.configuracoes.LimitePorInstitucaoCreateDTO;
 import controle.api.back_end.model.configuracoes.LimitePorInstituicao;
 
 public class LimitePorInstituicaoMapper {
 
-    public static LimitePorInstituicao toEntity(){
+    public static LimitePorInstituicao toEntity(LimitePorInstitucaoCreateDTO dto){
 
+        return null;
     }
 }
